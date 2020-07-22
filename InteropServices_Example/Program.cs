4c5a227b4cb230c827b2707c35dd99cbab7a25b2
Burak Hamdi TUFAN <43945916+thecodeprogram@users.noreply.github.com>
2020-07-22 13:46:00 +0300

    Add files via upload

---
+++ InteropServices_Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace InteropServices_Example
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Interop Services Example - Thecodeprogram";
+            StartAndWrite();
+            Console.ReadLine();
+        }
+
+        public static  void StartAndWrite()
+        {
+            IntPtr windowPointer = IntPtr.Zero;
+            int processCount = 100;
+            for (int i = 0; (i < processCount) && (windowPointer == IntPtr.Zero); i++)
+            {
+                windowPointer = FindWindow(null, "exampleNotepad.txt - Not Defteri");
+            }
+            if (windowPointer != IntPtr.Zero)
+            {
+                SetForegroundWindow(windowPointer);
+                Thread.Sleep(500);
+                SendKeys.SendWait("BurakHamdiTUFAN");
+                SendKeys.SendWait(Environment.NewLine);
+                SendKeys.SendWait("Thecodeprogram");
+                SendKeys.SendWait("{ENTER}");
+                SendKeys.Flush();
+            }
+        }
+
+    }
+}
